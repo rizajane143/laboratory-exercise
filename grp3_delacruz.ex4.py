015439b736848c426e2d778f15b4399991422d76
rizajane143 <rrdelacruz@paterostechnologicalcollege.edu.ph>
2026-03-11 22:06:09 +0800

    Create grp3_delacruz.ex4.py
    This script demonstrates various operations on integers, floats, and complex numbers, including addition, subtraction, multiplication, division, and exponentiation.

---
+++ grp3_delacruz.ex4.py
@@ -0,0 +1,47 @@
+a = 25
+b = 50
+
+print("\n----- INTEGER OPERATOR -----")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+print()
+
+c = 20.50
+d = 50.80
+
+print("\n-----FLOAT OPERATIONS-----")
+print("Addition:", c + d)
+print("Subtraction:", c - d)
+print("Multiplication:", c * d)
+print("Division:", c / d)
+print("Exponent:", c ** d)
+
+print()
+
+e = 3j
+f = 10j
+
+print("\n-----COMPLEX OPERATIONS-----")
+print("Addition:", e + f)
+print("Subtraction:", e - f)
+print("Multiplication:", e * f)
+print("Division:", e / f)
+
+num1 = 25000000
+num2 = 25_000_000
+
+print("\n",num1)
+print(num2)
+
+num_int = 10
+num_float = 5.5
+num_complex = 2j + 3j
+print()
+print("Value:", num_int, "Type:", type(num_int))
+print("Value:", num_float, "Type:", type(num_float))
+print("Value:", num_complex, "Type:", type(num_complex))
